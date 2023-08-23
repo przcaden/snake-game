1f847261c07810c101bf041c17e3dd8b95e1f100
Caden Perez <77550338+przcaden@users.noreply.github.com>
2023-08-23 12:54:27 -0500

    Prevent the player from moving backwards

--- src/key_handler.c
+++ src/key_handler.c
@@ -3,26 +3,36 @@
 #include <raylib.h>
 #include <stdio.h>
 
-void arrowKeyHandler(enum direction* key_pressed)
+void arrowKeyHandler(enum direction* moving_dir)
 {
+    enum direction key_pressed = *moving_dir;
     if (IsKeyPressed)
     {
+        // Get the current key pressed
         int tempKey = GetKeyPressed();
         switch (tempKey)
         {
             case KEY_UP:
-                *key_pressed = key_up;
+                key_pressed = key_up;
                 break;
             case KEY_DOWN:
-                *key_pressed = key_down;
+                key_pressed = key_down;
                 break;
             case KEY_LEFT:
-                *key_pressed = key_left;
+                key_pressed = key_left;
                 break;
             case KEY_RIGHT:
-                *key_pressed = key_right;
+                key_pressed = key_right;
                 break;
         }
+
+        // Prevent the player from moving backwards
+        if (key_pressed == key_left && *moving_dir == key_right ||
+            key_pressed == key_right && *moving_dir == key_left ||
+            key_pressed == key_up && *moving_dir == key_down ||
+            key_pressed == key_down && *moving_dir == key_up)
+            return;
+        *moving_dir = key_pressed;
     }
 }
 
--- src/key_handler.h
+++ src/key_handler.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 enum direction { key_up, key_down, key_left, key_right };
-void arrowKeyHandler(enum direction* key_pressed);
+void arrowKeyHandler(enum direction* moving_dir);
 bool startHandler();
 
 #endif
